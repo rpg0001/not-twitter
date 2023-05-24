affa8d539c548ba08d4b0d04011735734364ee64
penguin0001 <robynpgreer@gmail.com>
2023-05-24 18:25:57 +0100

    added bio and date fields to users

--- users/models.py
+++ users/models.py
@@ -3,8 +3,13 @@ from django.db import models
 
 
 # Create your models here.
+from django.utils import timezone
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default="")
+    date_joined = models.DateField(default=timezone.now)
 
     def __str__(self):
         return self.user.username
